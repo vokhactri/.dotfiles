467c2ebf072d9a0794480d3f774697b93cdfad6e
Tri Vo Khac <me@trivk.dev>
2026-07-14 11:30:43 +0700

    feat(zsh): configure claudex model routing

--- zsh/.config/zsh/config/alias.zsh
+++ zsh/.config/zsh/config/alias.zsh
@@ -7,7 +7,13 @@ alias la='lsd -lA'
 alias tree='lsd --tree'
 alias nano='micro'
 alias fetch='fastfetch'
-alias claudex='CLAUDE_CODE_SUBAGENT_MODEL=gpt-5.6-sol \
+alias claudex='ANTHROPIC_BASE_URL="$(pass tk_ai/cc_url)" \
+ANTHROPIC_AUTH_TOKEN="$(pass tk_ai/api_key)" \
+ANTHROPIC_DEFAULT_OPUS_MODEL=gpt-5.6-sol \
+ANTHROPIC_DEFAULT_SONNET_MODEL=gpt-5.6-terra \
+ANTHROPIC_DEFAULT_HAIKU_MODEL=gpt-5.6-luna \
+CLAUDE_CODE_DISABLE_1M_CONTEXT=1 \
+CLAUDE_CODE_SUBAGENT_MODEL=gpt-5.6-sol \
 CLAUDE_CODE_ALWAYS_ENABLE_EFFORT=1 \
 CLAUDE_CODE_MAX_TOOL_USE_CONCURRENCY=3 \
 ENABLE_TOOL_SEARCH=false \
