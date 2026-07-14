1b0a6e6d4225aa8eb14aa113cf63ce1f2a64890c
Tri Vo Khac <me@trivk.dev>
2026-07-14 15:33:21 +0700

    chore(zsh): disable Homebrew cask auto-updates on upgrade

--- zsh/.config/zsh/config/default.zsh
+++ zsh/.config/zsh/config/default.zsh
@@ -9,6 +9,7 @@ export SUDO_EDITOR="micro"
 
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME"/starship/starship.toml
 export OPENCODE_EXPERIMENTAL_BACKGROUND_SUBAGENTS=true
+export HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1
 
 export PNPM_HOME="$HOME/.local/share/pnpm"
 
