a75a04c814b05e1f8701eefddb8469133138c8bb
Tri Vo Khac <git@trivk.dev>
2026-08-03 16:44:59 +0700

    chore(hammerspoon): unload app name toast

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,28 +1,13 @@
 -- Reload: Hammerspoon menu -> Reload Config
 
 local config = require("config")
-local AppNameToast = require("modules.app_name_toast")
 local WindowHighlight = require("modules.window_highlight")
 
-local appNameToast = AppNameToast.new(config.appNameToast)
-local windowHighlight = WindowHighlight.new(config.windowHighlight, {
-  onWindowFocused = function()
-    if config.appNameToast.showOnFocus then
-      appNameToast:showOnFocus()
-    end
-  end,
-})
-
-appNameToast:setShouldShow(function()
-  return not windowHighlight:isMissionControlActive()
-end)
-
-appNameToast:start()
+local windowHighlight = WindowHighlight.new(config.windowHighlight)
 windowHighlight:start()
 
 -- Keep module state and watchers alive until the next reload.
 _G.hammerspoonModules = {
-  appNameToast = appNameToast,
   windowHighlight = windowHighlight,
 }
 
