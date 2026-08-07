55ca1573e5cb74775881a27642682d3234c4be82
Tri Vo Khac <git@trivk.dev>
2026-08-07 10:56:35 +0700

    refactor: remove deprecated Hammerspoon configuration and modules

--- zsh/.config/zsh/config/default.zsh
+++ zsh/.config/zsh/config/default.zsh
@@ -8,14 +8,12 @@ export VISUAL="micro"
 export SUDO_EDITOR="micro"
 
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME"/starship/starship.toml
-export OPENCODE_EXPERIMENTAL_BACKGROUND_SUBAGENTS=true
 export HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1
 
 export PNPM_HOME="$HOME/.local/share/pnpm"
 
 typeset -U path
 path=(
-  "$HOME/.opencode/bin"
   "$PNPM_HOME/bin"
   $path
 )
